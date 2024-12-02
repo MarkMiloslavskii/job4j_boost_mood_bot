50e341b3421964a8385ed39b3aa0d21e2e527f47
ivanvelikij <eldarvaleev01@gmail.com>
2024-12-02 15:26:21 +0300

    Интеграция Telegramm API [#505295]

--- src/main/java/ru/job4j/bmb/services/TgRemoteService.java
+++ src/main/java/ru/job4j/bmb/services/TgRemoteService.java
@@ -21,28 +21,17 @@ public class TgRemoteService extends TelegramLongPollingBot {
     }
 
     @Override
-    public String getBotToken() {
-        return botToken;
-    }
-
-    @Override
     public String getBotUsername() {
         return botName;
     }
 
     @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
-            SendMessage message = new SendMessage();
-            message.setChatId(chatId);
-            message.setText("Вы написали: " + messageText);
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
+        // Обработка обновлений
     }
 }
