c407a175dd3074955498d3a84927e2c0d84d47ea
ivanvelikij <eldarvaleev01@gmail.com>
2025-01-02 16:32:59 +0300

    Внедрение коллекций | Поставщики контекста [#505300 #525390]

--- src/main/java/ru/job4j/bmb/content/Content.java
+++ src/main/java/ru/job4j/bmb/content/Content.java
@@ -4,7 +4,7 @@ import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
 public class Content {
-    private final Long chatId;
+    private Long chatId;
     private String text;
     private InputFile photo;
     private InlineKeyboardMarkup markup;
@@ -12,6 +12,10 @@ public class Content {
 
     public Content(Long chatId) {
         this.chatId = chatId;
+    }
+
+    public Content() {
+
     }
 
     public String getText() {
