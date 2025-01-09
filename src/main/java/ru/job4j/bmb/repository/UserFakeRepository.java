27edc3d2020ce216d1d5f389cb2c8aa12f1f6872
ivanvelikij <eldarvaleev01@gmail.com>
2025-01-09 13:20:53 +0300

    Модели данных [#505302]

--- src/main/java/ru/job4j/bmb/repository/UserFakeRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserFakeRepository.java
@@ -4,15 +4,27 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import ru.job4j.bmb.model.User;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
 
 @Profile("test")
 @Repository
-public abstract class UserFakeRepository implements UserRepository {
+public class UserFakeRepository implements UserRepository {
     private Map<Long, User> userMap = new HashMap<>();
+
+    @Override
+    public <S extends User> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
 
     @Override
     public List<User> findAll() {
@@ -20,12 +32,53 @@ public abstract class UserFakeRepository implements UserRepository {
     }
 
     @Override
+    public Iterable<User> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(User entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends User> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
     public User findByClientId(Long clientId) {
         return userMap.get(clientId);
     }
 
-    public Object save(User user) {
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public <S extends User> S save(S user) {
         userMap.put(user.getClientId(), user);
-        return null;
+        return user;
     }
 }
