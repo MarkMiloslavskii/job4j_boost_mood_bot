1985bf91048d2d74bdec8df3e2779acafba660a5
ivanvelikij <eldarvaleev01@gmail.com>
2025-01-12 09:48:35 +0300

    MoodService - бизнес логика системы [#505299]

--- src/main/java/ru/job4j/bmb/recommendation/RecommendationEngine.java
+++ src/main/java/ru/job4j/bmb/recommendation/RecommendationEngine.java
@@ -3,6 +3,7 @@ package ru.job4j.bmb.recommendation;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.jvnet.hk2.annotations.Service;
+import ru.job4j.bmb.content.Content;
 
 @Service
 public class RecommendationEngine {
@@ -15,5 +16,9 @@ public class RecommendationEngine {
     @PreDestroy
     public void destroy() {
         System.out.println("RecommendationEngine уничтожен.");
+    }
+
+    public Content recommendFor(long chatId, Long moodId) {
+        return null;
     }
 }
