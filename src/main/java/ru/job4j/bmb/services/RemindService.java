01118987d815b33e0addc406b940d5437a163b29
ivanvelikij <eldarvaleev01@gmail.com>
2025-03-03 15:09:48 +0300

    @Service и @Repository | BotCommandHandler - обработка комманд [#505306 #533163]

--- src/main/java/ru/job4j/bmb/services/RemindService.java
+++ src/main/java/ru/job4j/bmb/services/RemindService.java
@@ -2,7 +2,6 @@ package ru.job4j.bmb.services;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import ru.job4j.bmb.repository.UserRepository;
 import ru.job4j.bmb.content.Content;
 
@@ -10,10 +9,10 @@ import static ru.job4j.bmb.content.Content.ContentType.TEXT;
 
 @Service
 public class RemindService {
-    private final TelegramBotService telegramBotService;
+    private final SentContent telegramBotService;
     private final UserRepository userRepository;
 
-    public RemindService(TelegramBotService telegramBotService, UserRepository userRepository) {
+    public RemindService(SentContent telegramBotService, UserRepository userRepository) {
         this.telegramBotService = telegramBotService;
         this.userRepository = userRepository;
     }
